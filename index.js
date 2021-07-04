d934f23f8b9362efb5034ec01cfdc3b47f9e199b
Michael Souter <souterm1@gmail.com>
2021-07-03 23:26:22 -0400

    Need to finish test, collaboration, and lisencing (prompt and image)

--- index.js
+++ index.js
@@ -5,36 +5,75 @@ inquirer
   .prompt([
     {
       type: "input",
-      message: "What is your project named?",
+      message: "What is your project name?",
       name: "name",
+    },
+
+    {
+      type: "editor",
+      message: "Describe your project",
+      name: "description",
+    },
+    {
+      type: "editor",
+      message: "Specify any installation instructions",
+      name: "install",
+      default: "No installation instructions",
+    },
+    {
+      type: "editor",
+      message: "Describe the end user experience",
+      name: "usage",
+    },
+
+    {
+      type: "input",
+      message: "Enter email address",
+      name: "email",
+    },
+    {
+      type: "input",
+      message: "Enter Github username",
+      name: "Github",
     },
   ])
   .then((data) => {
     const filename = `${data.name.toLowerCase().split("").join("")}.MD`;
-    const projectName = `### ${data.name}
+    const fileContent = `# ${data.name}
 
-## Table of Contents
+## Table of contents
 
-1. [Project Description](Project Description)
-2. Installation
-3. Usage
-4. License
-5. Contributing
-6. Tests
-7. Questions
+1. [Project Description](#Project_Description)
+2. [Installation](#Installation_Instructions)
+3. [Usage](#Usage)
+4. [License](#License)
+5. [Contribution](#Contribution)
+6. [Tests](#Tests)
+7. [Questions](#Questions)
 
-## Project Description <a name = "Project Description"></a>
+## Project Description
+${data.description}
 
 ## Installation Instructions
+${data.install}
 
 ## Usage
+${data.usage}
 
 ## License
 
-## Contributing
+## Contribution
 
-## Tests;`;
-    fs.writeFile(filename, projectName, (err) =>
+## Tests
+
+## Questions
+Got questions, or are interested in collaborating? 
+
+Github:https://github.com/${data.Github}
+
+Email: ${data.email}`;
+
+    fs.writeFile(filename, fileContent, (err) =>
       err ? console.log("Try Again") : console.log("Good Job")
     );
   });
